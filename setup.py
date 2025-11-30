5646a645a61b56db0f73e1733ab20ef924a2e5a3
Rashidmstar12 <user@example.com>
2025-11-30 14:20:00 +0530

    Add PyPI publication files

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tf-explorer",
+    version="1.1.0",
+    author="Rashidmstar12",
+    author_email="your.email@example.com",
+    description="A tool to explore TF binding sites for any gene using ENCODE and JASPAR.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rashidmstar12/TF-Explorer-ChIP-seq-Promoter-Scanner",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "requests",
+        "pandas",
+        "biopython",
+        "matplotlib",
+        "seaborn",
+        "mygene",
+        "pyyaml",
+        "streamlit"
+    ],
+    entry_points={
+        "console_scripts": [
+            "tf-explorer=tf_explorer.cli:main",
+        ],
+    },
+)
